8369679efab8beb79b3292a6ae0cf2a384dab9e3
Olga Komleva <olyakomleva@mail.ru>
2024-03-26 15:00:27 +0300

    add calculations to update serializers

--- kinosmena_backend/shifts/apps.py
+++ kinosmena_backend/shifts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ShiftsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shifts'
+    
+    # это для работы сигналов и сохранения БД на уровне модели по логике описанной в signals.py
+    # def ready(self):
+    #     import shifts.signals
